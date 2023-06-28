289160491f8a53b64f82d61c31c745c893b266f2
Maurice TIV <maurice_tiv@Maurices-MacBook-Pro.local>
2023-06-28 17:02:43 +0300

    Worked on Auth

---
+++ app/Http/Controllers/AUTH/AuthController.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace App\Http\Controllers\AUTH;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+/**
+* @group Auth Management
+* 
+* APIs for managing authentication
+*/
+
+class AuthController extends Controller
+{
+    /**
+     * Login user
+     * 
+     * This endpoint allows a user to login
+     * 
+     * @bodyParam email string required The email address or phone number of the user
+     * @bodyParam password string required The password of the user
+     * 
+     * @response {
+     * "success": true,
+     * "message": "User login successfully.",
+     * "data": {
+     * "id": 1,
+     * "name": "John Doe",
+     * "email": "john@test.com",
+     * "email_verified_at": null,
+     * "phone_number": "256700000000",
+     * "created_at": "2021-06-27T14:56:04.000000Z",
+     * "updated_at": "2021-06-27T14:56:04.000000Z",
+     * }
+     * }
+     * 
+     * @response 401 {
+     * "success": false,
+     * "message": "Unauthorised.",
+     * "data": null
+     * }
+     * 
+     * @response 422 {
+     * "message": "The given data was invalid.",
+     * "errors": {
+     * "email": [
+     * "The email field is required."
+     * ],
+     * "password": [
+     * "The password field is required."
+     * ]
+     * }
+     * }
+     * 
+     * @response 500 {
+     * "message": "Server Error."
+     * }
+     * 
+     * 
+     * 
+     */
+    public function login(Request $request)
+    {
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){ 
+            $user = Auth::user(); 
+            $success['token'] =  $user->createToken('token')->plainTextToken; 
+            $success['user'] =  $user;
+
+            return response()->json([
+                'success' => true,
+                'message' => 'User login successfully.',
+                'data' => $success
+            ], 200);
+        } 
+        else{ 
+            //Check if user exists with phone number
+            $user = User::where('phone_number', $request->email)->first();
+            if(Hash::check($request->password, $user->password)){
+                $success['token'] =  $user->createToken('token')->plainTextToken; 
+                $success['user'] =  $user;
+
+                return response()->json([
+                    'success' => true,
+                    'message' => 'User login successfully.',
+                    'data' => $success
+                ], 200);
+            }
+            else{
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Unauthorised.',
+                    'data' => null
+                ], 401);
+            }
+            return response()->json([
+                'success' => false,
+                'message' => 'Unauthorised.',
+                'data' => null
+            ], 401);
+        } 
+    }
+
+    /**
+     * Logout user
+     * 
+     * This endpoint allows a user to logout
+     * @authenticated
+     * 
+     * @response {
+     * "success": true,
+     * "message": "User logged out successfully.",
+     * "data": null
+     * }
+     * 
+     * @response 401 {
+     * "success": false,
+     * "message": "Unauthorised.",
+     * "data": null
+     * }
+     * 
+     */
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json([
+            'success' => true,
+            'message' => 'User logged out successfully.',
+            'data' => null
+        ], 200);
+    }
+
+    //Refresh token
+    /**
+     * Refresh token
+     * 
+     * This endpoint allows a user to refresh their token
+     * @authenticated
+     * 
+     * 
+     */
+    public function refresh(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return $request->user()->createToken('token')->plainTextToken;
+    }
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\API\AgentController;
 use App\Http\Controllers\API\DataController;
 use App\Http\Controllers\API\FarmerProfileController;
 use App\Http\Controllers\API\FPOController;
+use App\Http\Controllers\AUTH\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,19 +19,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/farmer/register', [FarmerProfileController::class, 'registerFarmer']);
-Route::get('/farmers', [DataController::class, 'getAllFarmers']);
-Route::get('/farmer/{id}', [DataController::class, 'getFarmer']);
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/login', 'login');
+    Route::post('/logout', 'logout');
+    Route::post('/refresh', 'refresh');
+});
 
-Route::get('/agents', [AgentController::class, 'index']);
-Route::post('/agent/register', [AgentController::class, 'store']);
-Route::get('/agent/{id}', [AgentController::class, 'show']);
-Route::post('/agent/{id}/update', [AgentController::class, 'update']);
+Route::middleware('auth:sanctum')->group( function () {
+    Route::post('/farmer/register', [FarmerProfileController::class, 'registerFarmer']);
+    Route::get('/farmers', [DataController::class, 'getAllFarmers']);
+    Route::get('/farmer/{id}', [DataController::class, 'getFarmer']);
 
-Route::get('/fpos', [FPOController::class, 'index']);
-Route::get('/fpos/summary', [FPOController::class, 'getFPOsSummary']);
-Route::post('/fpo/register', [FPOController::class, 'store']);
-Route::get('/fpo/{id}', [FPOController::class, 'show']);
-Route::post('/fpo/{id}/update', [FPOController::class, 'update']);
+    Route::get('/agents', [AgentController::class, 'index']);
+    Route::post('/agent/register', [AgentController::class, 'store']);
+    Route::get('/agent/{id}', [AgentController::class, 'show']);
+    Route::post('/agent/{id}/update', [AgentController::class, 'update']);
+
+    Route::get('/fpos', [FPOController::class, 'index']);
+    Route::get('/fpos/summary', [FPOController::class, 'getFPOsSummary']);
+    Route::post('/fpo/register', [FPOController::class, 'store']);
+    Route::get('/fpo/{id}', [FPOController::class, 'show']);
+    Route::post('/fpo/{id}/update', [FPOController::class, 'update']);
+});
+
 
 
