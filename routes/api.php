b2472d64475c6bde75ef99a8f70b4a34d5e3a448
Maurice TIV <maurice_tiv@Maurices-MacBook-Pro.local>
2023-07-06 10:14:09 +0300

    Added Agent search by code

--- routes/api.php
+++ routes/api.php
@@ -19,13 +19,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/agent/search', [AgentController::class, 'getSearchAgent']);
+
 Route::controller(AuthController::class)->group(function () {
     Route::post('/login', 'login');
     Route::post('/logout', 'logout');
     Route::post('/refresh', 'refresh');
 });
 
-Route::post('/agent/search', [AgentController::class, 'getSearchAgent']);
+
 
 Route::middleware('auth:sanctum')->group( function () {
     Route::post('/farmer/register', [FarmerProfileController::class, 'registerFarmer']);
