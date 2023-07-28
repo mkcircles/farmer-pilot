f47a26a541e6988d6848d0f7b7037a6dbe2541b8
Maurice TIV <maurice_tiv@Maurices-MacBook-Pro.local>
2023-07-28 12:12:41 +0300

    Resolved Reports Error

--- app/Console/Commands/ProcessReports.php
+++ app/Console/Commands/ProcessReports.php
@@ -70,8 +70,107 @@ class ProcessReports extends Command
             $qb->where($k, $v);
         }
         $farmers = $qb->get();
+        dd($farmers);
         //Create excel report 
+        header('Content-Type: text/csv');
+        header('Content-Disposition: attachment; filename="sample.csv"');
+
+        $header = array(
+            'Farmer Id',
+            'First name',
+            'Last name',
+            'DoB',
+            'Gender',
+            'Education Level',
+            'Phone number',
+            'id number',
+            'Marital status',
+            'district',
+            'county',
+            'sub_county',
+            'parish',
+            'village',
+            'FPO',
+            'Farmer cordinates',
+            'Next of kin',
+            'Next of kin contact',
+            'Next of kin address',
+            'Male members in_household',
+            'Female members in_household',
+            'Members above 18',
+            'Children below 5',
+            'School going children',
+            'Head of family',
+            'Agricultural activities Earnings',
+            'Non-agricultural activities Earnings',
+            'Do you have an account with an FI',
+            'Farm size',
+            'Farm size under agriculture',
+            'Land ownership',
+            'Type of farming',
+            'Crops grown',
+            'Animals kept',
+            'Last season estimated produce value',
+            'This season estimated produce value',
+            'rId',
+            'Consumer Device Id',
+            'Data captured by',
+            'Agent code',
+            'Agent name',
+    );
+        $fp = fopen('php://output', 'wb');
+        fputcsv($fp, $header, ',');
+
+        foreach($farmers as $farmer) {
+            $line = array(
+                $farmer->farmer_id,
+                $farmer->first_name,
+                $farmer->last_name,
+                $farmer->dob,
+                $farmer->gender,
+                $farmer->education_level,
+                $farmer->phone_number,
+                $farmer->id_number,
+                $farmer->marital_status,
+                $farmer->district,
+                $farmer->county,
+                $farmer->sub_county,
+                $farmer->parish,
+                $farmer->village,
+                $farmer->fpo->fpo_name,
+                $farmer->farmer_cordinates,
+                $farmer->next_of_kin,
+                $farmer->next_of_kin_contact,
+                $farmer->next_of_kin_address,
+                $farmer->male_members_in_household,
+                $farmer->female_members_in_household,
+                $farmer->members_above_18,
+                $farmer->children_below_5,
+                $farmer->school_going_children,
+                $farmer->head_of_family,
+                $farmer->agricultural_activities_earnings,
+                $farmer->non_agricultural_activities_earnings,
+                $farmer->do_you_have_an_account_with_an_FI,
+                $farmer->farm_size,
+                $farmer->farm_size_under_agriculture,
+                $farmer->land_ownership,
+                $farmer->type_of_farming,
+                $farmer->crops_grown,
+                $farmer->animals_kept,
+                $farmer->last_season_estimated_produce_value,
+                $farmer->this_season_estimated_produce_value,
+                $farmer->rId,
+                $farmer->consumerDeviceId,
+                $farmer->data_captured_by,
+                $farmer->agent->agent_code,
+                $farmer->agent->first_name.' '.$farmer->agent->last_name,
+            );
+
+            fputcsv($fp, $line, ',');
+        }
         
+        fclose($fp);
+
         
 
     }
