8e7b03f7f9a9d4fbad4a2618e4e999f45dddcff2
Maurice TIV <maurice_tiv@Maurices-MacBook-Pro.local>
2023-08-22 13:56:09 +0300

    Working on the Mailing feature

--- resources/views/mail/biometric_report.blade.php
+++ resources/views/mail/biometric_report.blade.php
@@ -1,6 +1,6 @@
-Hello Team,<br/>
+Hello Team,<br/><br/>
 
-Hope you are doing fine. Attached is the report of the biometrics captured in the fielf today. <br/>
+Hope you are doing fine. Attached is the report of the biometrics captured in the fielf today. <br/><br/>
 
 Regards;<br/>
 System
